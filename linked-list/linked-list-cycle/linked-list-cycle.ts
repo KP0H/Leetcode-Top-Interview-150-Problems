f62f2125b01d2aacb34402163948929ed4c8fb83
Dmitry [KP0H™] Pelevin <dmitry@pelevin.pro>
2025-08-23 17:39:54 +0300

    Comments

--- linked-list/linked-list-cycle/linked-list-cycle.ts
+++ linked-list/linked-list-cycle/linked-list-cycle.ts
@@ -1,14 +1,12 @@
-/**
- * Definition for singly-linked list.
- * class ListNode {
- *     val: number
- *     next: ListNode | null
- *     constructor(val?: number, next?: ListNode | null) {
- *         this.val = (val===undefined ? 0 : val)
- *         this.next = (next===undefined ? null : next)
- *     }
- * }
- */
+// Definition for singly-linked list.
+class ListNode {
+    val: number
+    next: ListNode | null
+    constructor(val?: number, next?: ListNode | null) {
+        this.val = (val===undefined ? 0 : val)
+        this.next = (next===undefined ? null : next)
+    }
+}
 
 function hasCycle(head: ListNode | null): boolean {
     if (!head || !head.next) return false;
